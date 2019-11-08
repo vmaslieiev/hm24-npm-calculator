69725aa0abece7bee861eee6fa97d41d6b00b080
Vadym Maslieiev <macleev93@gmail.com>
2019-11-08 15:10:10 +0200

    hm24 was added

---
+++ app.js
@@ -0,0 +1,6 @@
+const calc = require('./calculator')
+
+console.log(calc.add(2,3)) // 5
+console.log(calc.sub(2,3)) // -1
+console.log(calc.mult(2,3)) // 6
+console.log(calc.div(6,3)) // 2
---
+++ calculator/index.js
@@ -0,0 +1,12 @@
+const add = require('./add')
+const div = require('./div')
+const mult = require('./mult')
+const sub = require('./sub')
+
+
+module.exports = {
+  add,
+  div,
+  mult,
+  sub,
+}
